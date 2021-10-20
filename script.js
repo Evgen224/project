68fbc527cdd98bbf30507181cde839a6ea32a295
Ivan <example@gmail.com>
2021-10-20 17:16:36 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const category = 'toys';
+// console.log('https://someurl.com/' + category);
+
+// const user = 'Ivan';
+// alert (`Привет, ${user}`);
+
+// console.log('arr' + '- object');
+// console.log(4 + '- object');
+// console.log(4 + + ' - object');
+// console.log(4 + + '5');
+
+// let incr = 10,
+//     decr = 10;
+//     // ++incr;
+//     // --decr;
+//     console.log(++incr);
+//     console.log(--decr);
+
+//     console.log(5%2);
+
+//     console.log(2*4==8);
+
+//     console.log(2*4 == '8');
+//     console.log(2*4 ==='8');
+//     console.log(2*4 ===8);
+
+// const isChecked = false,
+//       isClose = false;
+// console.log(isChecked || !isClose);      
+
+// console.log(2 + 2 * 2 != 8);
+
+console.log(2 + 2 * 2 !== '6');
